c7c391f1e601ddd00fcac68b79763c417af83b8f
24750Montenegro <mon24750@uvg.edu.gt>
2025-01-21 08:07:03 -0600

    Javadoc for interface added

--- src/main/java/uvg/edu/IBlender.java
+++ src/main/java/uvg/edu/IBlender.java
@@ -1,11 +1,50 @@
 package uvg.edu;
 
+/**
+ * Interface representing a Blender with various functionalities.
+ */
 public interface IBlender {
+
+    /**
+     * Checks if the blender is on.
+     *
+     * @return true if the blender is on, false otherwise.
+     */
     public boolean isOn();
+
+    /**
+     * Checks if the blender is full.
+     *
+     * @return true if the blender is full, false otherwise.
+     */
     public boolean isFull();
+
+    /**
+     * Adds an ingredient to the blender.
+     *
+     * @param ingredient the ingredient to be added.
+     */
     public void addIngredient(String ingredient);
+
+    /**
+     * Increases the speed of the blender.
+     */
     public void increaseSpeed();
+
+    /**
+     * Decreases the speed of the blender.
+     */
     public void decreaseSpeed();
-    public  void fill();
+
+    /**
+     * Fills the blender to its maximum capacity.
+     */
+    public void fill();
+
+    /**
+     * Gets the current speed of the blender.
+     *
+     * @return the current speed of the blender.
+     */
     public int getSpeed();
 }
